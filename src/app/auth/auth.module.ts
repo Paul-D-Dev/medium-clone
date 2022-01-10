e47594834631e4bd805634fef3032247070637f3
Paul DAO <dao.van.paul@gmail.com>
2022-01-10 14:56:15 -0500

    FEAT Component Auth/Register set up reactive form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
