1a14daa80dafd2fca38bbd3e932ca973e4333c1d
Paul DAO <dao.van.paul@gmail.com>
2022-01-11 09:38:07 -0500

    FEAT Register Interface

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,7 +1,8 @@
 import { createAction, props } from '@ngrx/store';
+import { RegisterRequestInterface } from '../types/register-request.interface';
 import { AuthActionTypes } from './auth.action-types';
 
 export const registerAction = createAction(
   AuthActionTypes.REGISTER,
-  props<{ username: string; password: string; email: string }>()
+  props<{ request: RegisterRequestInterface }>()
 );
