dd822697b0c6a0286cd24aa3d092b1a67ec9c6ba
Paul DAO <dao.van.paul@gmail.com>
2022-01-12 14:07:20 -0500

    FEAT Selector isLoggedIn, isAnonymous, currentUser

--- src/app/auth/store/selectors.ts
+++ src/app/auth/store/selectors.ts
@@ -1,4 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+
 import { IAppState } from '../../shared/types/app-state.interface';
 import { IAuthState } from '../types/auth-state.interface';
 
@@ -15,4 +16,19 @@ export const isSubmittingSelector = createSelector(
 export const validationErrorsSelector = createSelector(
   authFeatureSelector,
   (authState: IAuthState) => authState.validationErrors
+);
+
+export const isLoggedInSelector = createSelector(
+  authFeatureSelector,
+  (authState: IAuthState) => authState.isLoggedIn
+);
+
+export const isAnonymousSelector = createSelector(
+  authFeatureSelector,
+  (authState: IAuthState) => authState.isLoggedIn === false
+);
+
+export const currentUserSelector = createSelector(
+  authFeatureSelector,
+  (authState: IAuthState) => authState.currentUser
 );
