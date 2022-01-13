e81486584d7769cb483be255b0ca3c03445e5ebd
Paul DAO <dao.van.paul@gmail.com>
2022-01-13 13:42:39 -0500

    FEAT Pagination component

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { environment } from '../../../../../../environments/environment';
 
 import { getFeedAction } from '../../store/actions/getFeed.action';
 import {
@@ -15,23 +17,45 @@ import { IGetFeedResponse } from '../../types/get-feed-response.interface';
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input('apiUrl') apiUrlProps: string;
   feed$: Observable<IGetFeedResponse | null>;
   error$: Observable<string | null>;
   isLoading$: Observable<boolean | null>;
+  limit = environment.limit;
+  baseUrl: string;
+  currentPage: number;
+  queryParasSubscription: Subscription;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.initializeValues();
+    this.initializeListeners();
     this.fetchData();
+  }
+
+  ngOnDestroy() {
+    this.queryParasSubscription.unsubscribe();
+  }
+
+  initializeListeners(): void {
+    this.queryParasSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params['page'] || '1');
+      }
+    );
   }
 
   initializeValues(): void {
     this.feed$ = this.store.pipe(select(feedSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.baseUrl = this.router.url.split('?')[0];
   }
 
   fetchData(): void {
--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -6,6 +6,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { ErrorMessageModule } from '../error-message/error-message.module';
 import { LoadingModule } from '../loading/loading.module';
+import { PaginationModule } from '../pagination/pagination.module';
 
 import { FeedComponent } from './components/feed/feed.component';
 import { FeedService } from './services/feed.service';
@@ -25,6 +26,7 @@ import * as FromFeedReducer from './store/reducers';
     RouterModule,
     ErrorMessageModule,
     LoadingModule,
+    PaginationModule,
   ],
   exports: [FeedComponent],
   providers: [FeedService],
