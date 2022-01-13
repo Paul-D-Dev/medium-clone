1b1862d1fdab34edb9cdfedd2351ac886c1b4b57
Paul DAO <dao.van.paul@gmail.com>
2022-01-13 10:21:23 -0500

    Feat interfaces for feed module

---
+++ src/app/shared/types/article.interface.ts
@@ -0,0 +1,14 @@
+import { IProfile } from './profile.interface';
+
+export interface IArticle {
+  title: string;
+  author: IProfile;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  description: string;
+  favorited: boolean;
+  favoritesCount: number;
+  slug: string;
+  tagList: string[];
+}
---
+++ src/app/shared/types/profile.interface.ts
@@ -0,0 +1,6 @@
+export interface IProfile {
+  username: string;
+  image: string;
+  bio: string | null;
+  following: boolean;
+}
