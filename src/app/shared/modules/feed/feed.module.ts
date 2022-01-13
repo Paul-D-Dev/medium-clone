1050f689d517b16277b55581d44035fa6b0013ab
Paul DAO <dao.van.paul@gmail.com>
2022-01-13 10:21:54 -0500

    Feat Feed module provide service

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,10 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FeedComponent } from './components/feed/feed.component';
+import { FeedService } from './services/feed.service';
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [FeedComponent],
+  providers: [FeedService],
 })
 export class FeedModule {}
